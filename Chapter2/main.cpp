9236dec8906dac8bebb8b0db17612fe6e9c5bf30
19402 <19402@DESKTOP-QR63P17>
2017-02-12 22:07:34 +0800

    第二章完毕

--- Chapter2/main.cpp
+++ Chapter2/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+constexpr int size();
 void main() {
 	//bool b = true;
 	//char ch = 'h';
@@ -48,6 +49,59 @@ void main() {
 	//int i = d;	//编译通过 waring
 	//int j = { d };	//编译不通过 error C11新特性
 
-	int i = 10;
+	//extern 多文件使用同一变量进行申明
 
+	//::访问全局作用域
+	//复合类型:
+	//		1.引用
+	//		2.指针:未初始化的指针是野指针;空指针使用nullptr;void*能够指向任意对象
+	
+	//复合类型的申明:
+	//		1.定义多个变量
+	//		2.指向指针的指针
+	//		3.指向指针的引用:从右向左读
+
+	//const限定:值无法改变final
+	//		1.初始化无关乎const
+	//		2.通过extern使用共同的const
+
+	//const引用:
+	//		1.将引用绑定在const上,引用必须为const
+	//		2.const引用可以绑定非const变量
+	//		3.const引用绑定在其他类型的变量上实质是绑定在强转的临时变量上,非const归为非法
+
+	//const指针:
+	//		1.将指针绑定在const上,指针必须为const
+	//		2.const指针可以绑定非const变量
+	//		3.const指针变量和const指针:const x *const y = zz;
+
+	//顶层/底层const:
+	//		1.顶层:指针本身是个常量(指针变量):一般的针对变量
+	//		2.底层:指向的对象是个常量:一般针对指向的值
+
+	//constexpr申明的变量或函数一定是个常量表达式
+	//constexpr int mf = 20;
+	//constexpr int re = size(); //非constexpr报错
+	
+	//constexpr指针:对变量有效,对指针指向的值无效,为顶层const
+
+	//处理类型:
+	//		1.类型别名:typedef,using
+	//		2.指针的类型别名
+	//			typedef char* pstring;
+	//			char ch = 'h';
+	//			const pstring cstr = &ch;
+	//			*cstr = 'y';
+	//			char p = 'z';
+	//			cstr = &p; //报错,所以const pstring是个顶层const
+	//		3.auto类型说明符:让编译器分析类型,并运算赋值,一语句中只能用同一种类型
+	//			auto会忽略掉顶层const,如需要要另行申明
+	//			auto引用:绑定字面值时非常量引用而绑定常量时是常量引用
+	//		4.decltype类型提示:只推断类型,不赋初值
+	//			decltype不忽略顶层const
+	//			decltype先运算类型后推断,且只挖掘一次
+	//			decltype多一层括号就是引用
+}
+constexpr int size() {
+	return 10;
 }
